61565adc66d227cced0480424af4683a90427e65
Stefan Luiken <stefanluiken@outlook.com>
2020-08-30 19:28:05 +0200

    Added about us page

--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
-# Create your views here.
+from .models import Listing
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
-def listing(request):
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context = {
+        'listings': paged_listings
+    }
+
+
+    return render(request, 'listings/listings.html', context)
+
+def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
 def search(request):
