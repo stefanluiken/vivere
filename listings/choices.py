22603bfc5e15448cbd15d50319a3ce4a3badc7b2
Stefan Luiken <stefanluiken@outlook.com>
2020-09-03 23:40:08 +0200

    implementing search

---
+++ listings/choices.py
@@ -0,0 +1,40 @@
+bedroom_choices = {
+  '1':1,
+  '2':2,
+  '3':3,
+  '4':4,
+  '5':5,
+  '6':6,
+  '7':7,
+  '8':8,
+  '9':9,
+  '10':10
+  }
+
+price_choices = {
+  '100000':'€100,000',
+  '200000':'€200,000',
+  '300000':'€300,000',
+  '400000':'€400,000',
+  '500000':'€500,000',
+  '600000':'€600,000',
+  '700000':'€700,000',
+  '800000':'€800,000',
+  '900000':'€900,000',
+  '1000000':'€1M+',
+}
+
+state_choices = {
+        'DR': 'Drenthe',
+        'FL': 'Flevoland',
+        'FR': 'Friesland',
+        'GE': 'Gelderland',
+        'GR': 'Groningen',
+        'LI': 'Limburg',
+        'NB': 'North Brabant',
+        'NH': 'North Holland',
+        'OV': 'Overijssel',
+        'UT': 'Utrecht',
+        'ZE': 'Zeeland',
+        'ZH': 'South Holland',
+}
